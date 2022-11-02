f2878ee77437bf49971d0b6310c1c2943db97045
firlysetya93 <113082391+firlysetya93@users.noreply.github.com>
2022-11-02 15:36:43 +0700

    tugas_7

---
+++ function2.sh
@@ -0,0 +1,13 @@
+#mendeklarasikan fungsi
+function nama {
+ echo "Siapa namamu?"
+ read nama
+}
+function npm {
+ echo "Sebutkan npm mu"
+ read npm
+ echo -e "Hai $nama dengan npm $npm, selemat datang \n di praktikum sistem operasi yang seru ini ya!"
+}
+#memanggil fungsi
+nama 
+npm
---
+++ tugas_7.sh
@@ -0,0 +1,18 @@
+panjang() {
+ echo "Masukkan panjang : "
+ read panjang
+}
+lebar() {
+ echo "Masukkan lebar : "
+ read lebar
+}
+luas() {
+ let persegi=$panjang*$lebar
+ echo "luas persegi : $persegi "
+ read luas persegi
+}
+panjang
+lebar
+luas
+
+
